25498e9276ee639a35517cc21883e6478beab42a
vergilooos <mehrshad.nknz@yahoo.com>
2025-06-25 19:28:40 +0200

    Domain is ready

---
+++ AirportRegistration/AirportRegistration.Domain/Repositories/IPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using AirportRegistration.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportRegistration.Domain.Repositories
+{
+    public interface IPersonRepository
+    {
+        Task<List<Person>> GetAllAsync();
+        Task<Person?> GetByIdAsync(Guid id);
+        Task AddAsync(Person person);
+        Task UpdateAsync(Person person);
+        Task DeleteAsync(Guid id);
+    }
+}
